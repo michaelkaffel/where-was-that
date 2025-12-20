a9cc801470aefe7d497dbb0ba47e490cc742b095
MichaelKaffel <michaeldkaffel@gmail.com>
2025-12-19 18:33:17 -0800

    fixed placeholder image loading issue in PreviewCard.js

--- src/components/PreviewCard.js
+++ src/components/PreviewCard.js
@@ -1,13 +1,20 @@
 import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import placeHolderImg from '../app/images/hikesPlaceholder.png'
 
 
 const PreviewCard = ({ item }) => {
     const { image, title, description, location } = item
 
+    let imageInsert;
+
+    if (!image) {
+        imageInsert = placeHolderImg   
+    } else imageInsert = image
+
     return (
         <Card className='m-3'>
-            <img alt="Sample" src={image}/>
+            <img alt="Sample" src={imageInsert}/>
             <CardBody>
                 <CardTitle tag="h2">{title}</CardTitle>
                 <CardSubtitle className="mb-2 text-muted" tag="h6">
