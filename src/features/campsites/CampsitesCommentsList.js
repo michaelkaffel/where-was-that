6e82b08742801b05417d6bbc159689d93892abf2
MichaelKaffel <michaeldkaffel@gmail.com>
2025-12-22 14:15:26 -0800

    conditional rendering for CampsitesCommentsList.js

--- src/features/campsites/CampsitesCommentsList.js
+++ src/features/campsites/CampsitesCommentsList.js
@@ -7,14 +7,26 @@ import CampsiteComment from './CampsiteComment';
 const CampsitesCommentsList = ({ campsiteId }) => {
     const comments = useSelector(selectCampsitesCommentsbyCampsiteId(campsiteId));
 
+    if (comments && comments.length > 0) {
+        return (
+            <Col>
+                <h4>Comments</h4>
+                {comments.map((comment) => {
+                    return <CampsiteComment key={comment.id} comment={comment} />
+                })}
+            </Col>
+        )
+    }
+
     return (
-        <Col>
+        <Col md='5' className='m-1'>
             <h4>Comments</h4>
-            {comments.map((comment) => {
-                return <CampsiteComment key={comment.id} comment={comment} />
-            })}
+            <p>There are no comments for this campsite yet.</p>
+            
+            {/* <CommentForm campsiteId={campsiteId}/> */}
         </Col>
     )
+
 }
 
 export default CampsitesCommentsList;
