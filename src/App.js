0b6100d7b461e42490631311f2c00b43a0461f6b
MichaelKaffel <michaeldkaffel@gmail.com>
2025-12-26 16:16:32 -0800

    clean up commit before deployment

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
     <>
       
       <NavigationBar />
-      
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='add-locations' element={<AddLocationsPage/>}/>
